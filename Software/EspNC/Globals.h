baf6569dd0cd7ebf3ca56132929d23973ba0df4f
forbidden-kiwi <162729763+forbidden-kiwi@users.noreply.github.com>
2024-10-27 12:50:13 +0100

    Add files via upload

---
+++ Software/EspNC/Globals.h
@@ -0,0 +1,17 @@
+#ifndef Globals_h
+#define Globals_h
+
+// Pins to HV5622 shift registers
+#define PIN_HV_LE 0
+#define PIN_HV_BL 2
+#define PIN_HV_DATA 12
+#define PIN_HV_CLK 13
+
+// Pin to DC/DC 170V enable pin
+#define PIN_HV_EN 14
+
+// Pin to Neopixel LED's
+#define PIN_NEOPIXEL 10
+#define LED_COUNT 12
+
+#endif
